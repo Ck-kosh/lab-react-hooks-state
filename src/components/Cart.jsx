18e129529d1066fbc6a1a2b2ab832dc9f0c29301
Ck-kosh <elias.cheruiyot@student.moringaschool.com>
2026-05-04 11:28:48 +0300

    modify

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,10 +6,10 @@ const Cart = ({ cart }) => {
       <h2>Shopping Cart</h2>
       <ul>
         {/* TODO: Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
-        cart.map((item, index) => (
+        {cart.map((item, index) => (
           <li key={index}>
             {item.name} is in your cart.</li>
-        ))
+        ))}
       </ul>
     </div>
   )
